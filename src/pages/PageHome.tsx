2abce33047493d8e32ca931612d0d284d5cd3e3d
Dasajton <david.sajitz.94@gmail.com>
2024-02-09 17:45:49 +0100

    headline for Page

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,5 +1,11 @@
 const PageHome = () => {
-  return <div>PageHome</div>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center">
+        Organize your Gym Progress
+      </h1>
+    </>
+  );
 };
 
 export default PageHome;
