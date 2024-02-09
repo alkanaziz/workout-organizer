5b204e2e0b107bad55efc95d7bdaba8d6b7e4df5
Dasajton <david.sajitz.94@gmail.com>
2024-02-09 17:13:36 +0100

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="w-full h-16 bg-slate-950 flex items-center justify-between px-4 my-4 rounded-lg">
-      <h1 className="font-medium">Workout Organizer</h1>
+    <nav className="w-full h-16 bg-slate-950 flex items-center justify-center md:justify-between px-4 my-4 rounded-lg">
+      <h1 className="hidden md:block font-medium">Workout Organizer</h1>
       <ul className="flex space-x-6">
         {links.map((link) => (
           <li key={link.to}>
