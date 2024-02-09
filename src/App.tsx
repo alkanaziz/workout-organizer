9ccb7945d8dac08394455febefbbf6a486a338c2
Dasajton <david.sajitz.94@gmail.com>
2024-02-09 17:46:03 +0100

    Outlet wrapped in main-tag

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ const App = () => {
   return (
     <div className="w-11/12 md:w-10/12 mx-auto">
       <Navbar />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 };
