5b2bf7a17d23a0ac68578611c92e7d487570963c
Dasajton <david.sajitz.94@gmail.com>
2024-02-13 19:47:07 +0100

    changed places (workout -> home) (profile -> workout)

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,9 +1,9 @@
+import { CreateWorkout } from "../components";
+
 const PageHome = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">
-        Organize your Gym Progress
-      </h1>
+      <CreateWorkout />
     </>
   );
 };
